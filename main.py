c77662133850c9996c374fcb223f0c49eeea5574
mr-hoffman <hoffmant@hilbertk12.org>
2025-02-05 09:55:14 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# draw_7()
+
+
+
+# stars_and_stripes()
+
+
+
+# increasing_triangle()
+
+
+
+# vertical_stars_and_stripes()
+
+
+
+# bordered_square()
+
+
+
+# balanced_triangle()
+
+
+
+# centered_triangle()
